e6d11f198fc1b65e6356753c383ed7cf8e889a8e
Stefan2834 <iosifs617@gmail.com>
2022-10-17 19:11:22 +0300

    EditPage

---
+++ MyPortofolio.anime.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    anime({
+        targets: '#svgGroup',
+        strokeDashoffset: [1000, 0],
+        easing: 'easeInOutSine',
+        duration: 6000,
+        delay: function(el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: false,
+    },)
+    
+})
+
--- MyPortofolio.jquery.js
+++ MyPortofolio.jquery.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-    $("#page").fadeIn(2000);
-    $("#loading").fadeOut(2000, function() {
-        $("body").css("overflow-y", "scroll");
-    });
+    setTimeout(function() {
+        $("#page").fadeIn(2000);
+        $("#loading").fadeOut(2000, function() {
+            $("body").css("overflow-y", "scroll");
+        });
+    }, 2000)
 });
